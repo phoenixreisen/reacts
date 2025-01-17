6934b24b54f5d51d8b34d3757777c44290ab3c39
André Presser <a.presser@phoenixreisen.com>
2025-01-17 14:50:44 +0100

    feat(Modal): kommata entfernt in markup, markup aufgeraeumt, erlaube klick auf hintergrund zum schliessen des modals

--- src/modal/docs/demo.tsx
+++ src/modal/docs/demo.tsx
@@ -1,46 +1,60 @@
-import { Item1 } from './demo.content';
-import React, { useState } from 'react';
-import Modal from '../modal.r';
+import { Item1 } from "./demo.content";
+import React, { useState } from "react";
+import Modal from "../modal.r";
 
 //--- Komponente -----
 
 export const Demo = () => {
-    const [show, setShow] = useState(false);
+  const [show, setShow] = useState(false);
 
-    return (
-        <div className="modal-demo">
-            <div className="box">
-                <a href="#" title="Modal aufrufen" className="btn btn--link"
-                    onClick={(e) => { e.preventDefault(); setShow(true) }}>
-                    Klick mich!
-                </a>
+  return (
+    <div className="modal-demo">
+      <div className="box">
+        <a
+          href="#"
+          title="Modal aufrufen"
+          className="btn btn--link"
+          onClick={(e) => {
+            e.preventDefault();
+            setShow(true);
+          }}
+        >
+          Klick mich!
+        </a>
+      </div>
+
+      {show && (
+        <Modal
+          title="Modal Demo"
+          withCloseText={true}
+          toggle={() => setShow(false)}
+          preventOverflowHidden
+          allowBgClickToClose
+          content={
+            <div>
+              <Item1 />
             </div>
-
-            {show &&
-                <Modal title="Modal Demo"
-                    withCloseText={true}
-                    toggle={() => setShow(false)}
-                    content={
-                        <div>
-                            <Item1 />
-                        </div>
-                    }
-                    footer={
-                        <div className="tr">
-                            <button className="btn btn--secondary mr1"
-                                onClick={() => setShow(false)}>
-                                Abbrechen
-                            </button>
-                            <button className="btn btn--primary ml1"
-                                onClick={() => setShow(false)}>
-                                Verbindlich buchen
-                            </button>
-                        </div>
-                    }>
-                </Modal>
-            }
-        </div>
-    );
+          }
+          footer={
+            <div className="tr">
+              <button
+                className="btn btn--secondary mr1"
+                onClick={() => setShow(false)}
+              >
+                Abbrechen
+              </button>
+              <button
+                className="btn btn--primary ml1"
+                onClick={() => setShow(false)}
+              >
+                Verbindlich buchen
+              </button>
+            </div>
+          }
+        ></Modal>
+      )}
+    </div>
+  );
 };
 
 export default Demo;
--- src/modal/modal.r.tsx
+++ src/modal/modal.r.tsx
@@ -1,82 +1,78 @@
-import React, { FunctionComponent, useEffect } from 'react';
-import Sizes from './modal.sizes';
+import React, { FunctionComponent, useEffect } from "react";
+import Sizes from "./modal.sizes";
 
 //--- Types -----
 
 type Props = {
-    size?: Sizes,
-    title: string,
-    footer?: React.ReactNode,
-    content?: React.ReactNode,
-    children?: React.ReactNode,
-    withCloseText?: boolean,
-    toggle?: (e?) => void
-}
+  size?: Sizes;
+  title: string;
+  footer?: React.ReactNode;
+  content?: React.ReactNode;
+  children?: React.ReactNode;
+  withCloseText?: boolean;
+  preventOverflowHidden?: boolean;
+  allowBgClickToClose?: boolean;
+  toggle?: (e?) => void;
+};
 
 //--- Komponente -----
 
 export const Modal: FunctionComponent<Props> = (props) => {
-    const { size, content, children, toggle } = props;
+  const { size, content, children, toggle } = props;
 
-    useEffect(() => {
-        if(!content && !React.Children.count(children)) {
-            throw 'Modal without Content?';
-        } else if(size && ![...Object.values(Sizes)].includes(size)) {
-            throw 'Unknown size value given';
-        }
-    }, []);
+  useEffect(() => {
+    if (!content && !React.Children.count(children)) {
+      throw "Modal without Content?";
+    } else if (size && ![...Object.values(Sizes)].includes(size)) {
+      throw "Unknown size value given";
+    }
 
-    useEffect(() => {
-        document.addEventListener('keydown', (e:KeyboardEvent) => {
-            if(e.keyCode === 27) {
-                toggle?.();
-            }
-        });
-    }, []);
+    function onKeyDown(e) {
+      // esc key down
+      if (e.keyCode === 27) {
+        toggle?.();
+      }
+    }
+    document.addEventListener("keydown", onKeyDown);
 
-    useEffect(() => {
-        const $body = document.querySelector('body') as HTMLElement;
+    const $body = document.querySelector("body") as HTMLElement;
+    if ($body && !props.preventOverflowHidden) {
+      $body.style.overflow = "hidden";
+    }
 
-        if($body) {
-            $body.style.overflow = 'hidden';
-        }
-        return () => {
-            if($body) {
-                $body.style.overflow = '';
-            }
-        };
-    }, []);
+    return () => {
+      document.removeEventListener("keydown", onKeyDown);
+      if ($body) {
+        $body.style.overflow = "";
+      }
+    };
+  }, []);
 
-    return (<>
-        <article className={`modal modal--visible ${props.size || ''}`}>
-            <div className="modal__header">
-                <span className="modal__headline">
-                    { props.title || '' }
-                </span>
-                {props.toggle &&
-                    <a className="modal__toggle" href="#" onClick={props.toggle}>
-                        {props.withCloseText &&
-                            <span className="desktop-only">
-                                schließen
-                            </span>
-                        }
-                        <i className="fas fa-times-circle"></i>
-                    </a>
-                }
-            </div>
+  return (
+    <>
+      <div className={`modal modal--visible ${props.size || ""}`}>
+        <div className="modal__header">
+          <span className="modal__headline">{props.title || ""}</span>
+          {props.toggle && (
+            <a className="modal__toggle" href="#" onClick={props.toggle}>
+              {props.withCloseText && (
+                <span className="desktop-only">schließen</span>
+              )}
+              <i className="fas fa-times-circle"></i>
+            </a>
+          )}
+        </div>
 
-            <div className="modal__content">
-                { props.content || props.children }
-            </div>
+        <div className="modal__content">{props.content || props.children}</div>
 
-            {props.footer &&
-                <div className="modal__footer tr">
-                    { props.footer }
-                </div>
-            }
-        </article>,
-        <article className="modal__bg" />,
-    </>);
+        {props.footer && <div className="modal__footer tr">{props.footer}</div>}
+      </div>
+      <div
+        onClick={props.allowBgClickToClose && toggle}
+        className="modal__bg"
+      />
+    </>
+  );
 };
 
 export default Modal;
